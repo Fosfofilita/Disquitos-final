07fe0238abce71762ac05105454efd3226dfa407
Fosfofilita <redondolucila20@gmail.com>
2026-06-24 18:53:47 -0300

    agrego caso de prueba

--- teste2e.py
+++ teste2e.py
@@ -9,12 +9,11 @@ def probar_flujo_compra_completo():
     try:
         driver.get("http://127.0.0.1:5000/")
         time.sleep(1)
-        #busco el botón gris usando xpath por texto exacto que muestra
+        
         boton_vaciar = driver.find_element(By.XPATH, "//button[contains(text(), 'Vaciar Carrito')]")
         boton_vaciar.click()
         time.sleep(1)
         
-        #selecciono primer producto de la lista  
         primer_producto = driver.find_element(By.CLASS_NAME, "producto")
         boton_agregar = primer_producto.find_element(By.TAG_NAME, "button")
         
@@ -27,7 +26,7 @@ def probar_flujo_compra_completo():
         
         print(f"Total detectado: ${texto_total}")
         assert texto_total == "1000", f"Error: se esperaba 1000 pero se obtuvo {texto_total}"
-        print("Prueba 1 exitosa") #valido que aparezca 1000 en la pantalla 
+        print("Prueba 1 exitosa") 
         
     except Exception as e:
         print(f"La prueba 1 falló debido a: {e}")
@@ -47,20 +46,15 @@ def probar_compra_masiva_diez_discos():
         boton_vaciar.click()
         time.sleep(1)
         
-        print("Prueba 2: comprando 10 unidades de arise")
-        # //div[contains(@class, "producto")] busca cualquier tarjeta de producto en la pantalla
-        # .//*[contains(text(), "Arise")]]'  se queda con la tarjeta que adentro tenga el texto arise 
-        # el * indica que no importa si el título es un h3 h4
-        # guarda esa tarjeta específica en la variable tarjeta arise.
+        print("Prueba 2: comprando 10 unidades de Arise (Búsqueda Dinámica)")
+        
+        # El robot busca la tarjeta que contiene el texto "Arise" sin importar su posición
         tarjeta_arise = driver.find_element(By.XPATH, "//div[contains(@class, 'producto') and .//*[contains(text(), 'Arise')]]")
-     
-        # Buscá la etiqueta button de tarjeta_arise
         boton_agregar_arise = tarjeta_arise.find_element(By.TAG_NAME, "button")
-       
 
         for i in range(10):
             boton_agregar_arise.click()
-            time.sleep(0.3)  # agrego hasta llegar a 10 con un delay entre click
+            time.sleep(0.3)  
             
         time.sleep(2)
         
@@ -75,11 +69,71 @@ def probar_compra_masiva_diez_discos():
         print(f"La prueba 2 falló debido a: {e}")
         
     finally:
-        time.sleep(3)
+        time.sleep(1)
+        driver.quit()
+
+
+def probar_finalizar_compra_y_emision_ticket():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    
+    try:
+        driver.get("http://127.0.0.1:5000/")
+        time.sleep(1)
+        
+        #vacio carrito
+        boton_vaciar = driver.find_element(By.XPATH, "//button[contains(text(), 'Vaciar Carrito')]")
+        boton_vaciar.click()
+        time.sleep(1)
+        
+        print("Prueba 3: flujo de compra y reseteo de Tienda")
+        
+        # agrego disco arise
+        tarjeta_arise = driver.find_element(By.XPATH, "//div[contains(@class, 'producto') and .//*[contains(text(), 'Arise')]]")
+        tarjeta_arise.find_element(By.TAG_NAME, "button").click()
+        time.sleep(1)
+        
+        # busca y finaliza compra 
+        boton_finalizar = driver.find_element(By.ID, "btn-finalizar")
+        boton_finalizar.click()
+        time.sleep(2) 
+        
+        # buscamos el contenedor ticket
+        contenedor_ticket = driver.find_element(By.ID, "detalle-ticket")
+        texto_ticket = contenedor_ticket.text
+        
+        print(f"Texto detectado en el ticket final:\n{texto_ticket}")
+        
+        # valido que  el ticket muestre el total y mencione al producto
+        assert "Tu compra: $2000" in texto_ticket, "Error: El ticket no muestra el encabezado de precio correcto"
+        assert "Arise" in texto_ticket, "Error: El desglose del ticket no menciona el producto comprado"
+        print("Visualizacion de ticket: OK")
+        
+        # verifico que la tienda vuelva a $0
+        boton_volver = driver.find_element(By.XPATH, "//button[contains(text(), 'Volver a comprar')]")
+        boton_volver.click()
+        time.sleep(1)
+        
+        # valido el 0
+        total_recargado = driver.find_element(By.ID, "total-compra").text
+        assert total_recargado == "0", f"Error: Al volver a la tienda el total debió ser 0 pero ecnontro {total_recargado}"
+        print("Reseteo de tienda a 0 : OK")
+        
+        print("Prueba 3 exitosa")
+        
+    except Exception as e:
+        print(f"La prueba 3 falló debido a: {e}")
+        
+    finally:
+        time.sleep(2)
         driver.quit()
 
 if __name__ == '__main__':
     probar_flujo_compra_completo()
-    probar_compra_masiva_diez_discos()
 
-    #python teste2e.py
+    probar_compra_masiva_diez_discos()
+    
+    probar_finalizar_compra_y_emision_ticket()
+
+
+#python teste2e.py
